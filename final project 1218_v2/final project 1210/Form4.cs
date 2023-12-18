4f789eb1c604c4afeb91b1daa60e1439c6f6ae8c
5TooTired <105542603+5TooTired@users.noreply.github.com>
2023-12-19 01:39:01 +0800

    Add files via upload

---
+++ final project 1218_v2/final project 1210/Form4.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final_project_1210
+{
+ 
+    public partial class Form4 : Form
+    {
+        public int recievefeed = 0;
+        public Form4(int feed)
+        {
+            InitializeComponent();
+            recievefeed = feed;
+        }
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            bg.BackColor = Color.Transparent;
+            groupBox1.Parent = bg;
+            animal.Parent = bg;
+            label1.Text = "我的飼料:" + recievefeed.ToString();   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (recievefeed == 0)
+            {
+                MessageBox.Show("沒有飼料了喔!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                recievefeed--;
+                label1.Text = "我的飼料:"+recievefeed.ToString(); 
+            }
+        }
+        
+        int move1, move2;
+        // Create an array of image paths or Image objects
+        private Image[] images = { Properties.Resources.圖片10, Properties.Resources.圖片11,Properties.Resources.圖片12, Properties.Resources.圖片13, Properties.Resources.圖片14 };
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rd1 = new Random();
+            move1=rd1.Next(-100, 100);
+            Random rd2 = new Random();
+            move2=rd2.Next(-15, 15);
+            Random random = new Random();
+            int randomIndex = random.Next(images.Length);
+            if (animal.Location.X <= 615 && animal.Location.X >= -3 && animal.Location.Y >= 102 && animal.Location.Y <= 269)
+            {
+                animal.Left = move1 + animal.Location.X;
+                animal.Top = move2 + animal.Location.Y;
+                animal.Image = images[randomIndex];
+            }
+        }
+        
+    }
+}
---
+++ final project 1218_v2/final project 1210/account.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace final_project_1210
+{
+    partial class account
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "我的帳號";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "目前已累積:   隻動物";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "等級:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 194);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "正在飼養的動物";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 83);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 380);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "已蒐集的動物";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 476);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "未解鎖的動物";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "使用者名稱:";
+            // 
+            // account
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 570);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "account";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "我的帳號";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
