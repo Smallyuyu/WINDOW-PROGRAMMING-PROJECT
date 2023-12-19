7e12baa2b24db71a07d0fde6db64961582fc4a62
Smallyuyu <86581722+Smallyuyu@users.noreply.github.com>
2023-12-19 19:31:17 +0800

    Add files via upload

---
+++ final project 1219/final project 1210/Form4.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace final_project_1210
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.animal = new System.Windows.Forms.PictureBox();
+            this.bg = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.animal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "我的飼料:0";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(516, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "餵食";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(634, 73);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(457, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 35);
+            this.label2.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // animal
+            // 
+            this.animal.Image = global::final_project_1210.Properties.Resources.圖片12;
+            this.animal.Location = new System.Drawing.Point(302, 225);
+            this.animal.Name = "animal";
+            this.animal.Size = new System.Drawing.Size(112, 191);
+            this.animal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.animal.TabIndex = 4;
+            this.animal.TabStop = false;
+            // 
+            // bg
+            // 
+            this.bg.Image = global::final_project_1210.Properties.Resources.gettyimages_181156250_612x612;
+            this.bg.Location = new System.Drawing.Point(-16, -7);
+            this.bg.Name = "bg";
+            this.bg.Size = new System.Drawing.Size(743, 467);
+            this.bg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bg.TabIndex = 2;
+            this.bg.TabStop = false;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 455);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.animal);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bg);
+            this.Name = "Form4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "動物園";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.animal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox bg;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox animal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
